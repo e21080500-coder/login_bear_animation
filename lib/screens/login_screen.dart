6a45ec0d56ceef40bf6bf41bafb9df9635ccdb7f
Angel Antonio Serrano Molina <e21080500@merida.tecnm.mx>
2026-09-10 19:24:12 -0600

    feat:(login):add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,32 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
 class LoginScreen extends StatefulWidget {
-  const new({super.key});
+  const LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //control para mostrar u ocultar la contraseña
+  bool _obscure = true;
   @override
   Widget build(BuildContext context) {
-    //Para obtener el tamano de la pantalla
+    // Para obtener el tamaño de la pantalla
     final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
               SizedBox(
                 width: size.width,
                 height: 200,
-                child: RiveAnimation.asset('login-bear.riv')
+                child: RiveAnimation.asset(
+                  'assets/login-bear.riv',
+                ),
               ),
-            ]
+              //para separar espacio
+              SizedBox(height: 10),
+              //campo de texto para email
+              TextField(
+                //para mostrar el tipo de teclado
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              //campo de texto para contraseña
+              TextField(
+                obscureText: _obscure,
+                //para mostrar el tipo de teclado
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: 'Contraseña',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    //operador ternario para mostrar el icono de visibilidad
+                    icon: Icon(
+                      _obscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscure = !_obscure;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
           ),
-          ),
-      )     
+        ),
+      ),
     );
   }
 }
